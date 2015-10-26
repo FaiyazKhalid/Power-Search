8280a81db4b11db7be896fe6e5d8abf9f8fca106
mudroljub <mudroljub@gmail.com>
2015-10-26 13:07:59 +0100

    createImageUrl

--- app.js
+++ app.js
@@ -1,255 +1,262 @@
-(function() {
+(function () {
 
-    // osvetliti izabrani projekat, mozda outline
-    // sacuvati podesavanja (jezik, broj rez) u local storage
-    // veliku sliku za glavni clanak
-    // commons treba da pretrazuje i otvara fajlove, ne clanke
-    // primer paramUrl u dokumentaciju
-    // bug: trazim zen na wiki, pa na recniku, pa opet na wiki, a lead ostane sa recnika
+	// osvetliti izabrani projekat, mozda outline
+	// sacuvati podesavanja (jezik, broj rez) u local storage
+	// veliku sliku za glavni clanak
+	// commons treba da pretrazuje i otvara fajlove, ne clanke
+	// primer paramUrl u dokumentaciju
+	// bug: trazim zen na wiki, pa na recniku, pa opet na wiki, a lead ostane sa recnika
 
-    /*
-        za naziv slike vraca url:
-        https://en.wikipedia.org/w/api.php?action=query&titles=File:Albert%20Einstein%20Head.jpg&prop=imageinfo&iiprop=url
-        mozemo dodati i zeljenu sirinu: &iiurlwidth=220
+	/*
+	    za naziv slike vraca url:
+	    https://en.wikipedia.org/w/api.php?action=query&titles=File:Albert%20Einstein%20Head.jpg&prop=imageinfo&iiprop=url
+	    mozemo dodati i zeljenu sirinu: &iiurlwidth=220
 
-        vraca nadjene slike za trazeni termin:
-        https://en.wikipedia.org/w/api.php?action=query&list=allimages&aiprop=url&format=json&ailimit=10&aifrom=Albert
+	    vraca nadjene slike za trazeni termin:
+	    https://en.wikipedia.org/w/api.php?action=query&list=allimages&aiprop=url&format=json&ailimit=10&aifrom=Albert
 
-        alternativni commonsapi:
-        https://tools.wmflabs.org/magnus-toolserver/commonsapi.php
-        vraca info o slici i url:
-        https://tools.wmflabs.org/magnus-toolserver/commonsapi.php?image=Albert_Einstein_Head.jpg
+	    alternativni commonsapi:
+	    https://tools.wmflabs.org/magnus-toolserver/commonsapi.php
+	    vraca info o slici i url:
+	    https://tools.wmflabs.org/magnus-toolserver/commonsapi.php?image=Albert_Einstein_Head.jpg
 
-        lista jezika:
-        https://phabricator.wikimedia.org/diffusion/MW/browse/master/languages/Names.php
-        https://meta.wikimedia.org/wiki/List_of_Wikipedias
-        https://en.wikipedia.org/wiki/List_of_Wikipedias
-    */
+	    lista jezika:
+	    https://phabricator.wikimedia.org/diffusion/MW/browse/master/languages/Names.php
+	    https://meta.wikimedia.org/wiki/List_of_Wikipedias
+	    https://en.wikipedia.org/wiki/List_of_Wikipedias
+	*/
 
-    'use strict';
-    angular
-        .module("wikiModul", ['ngSanitize'])
-        .controller('WikiController', WikiController)
-        .directive('autofocus', ['$timeout', autofocus]);
+	'use strict';
+	angular
+		.module("wikiModul", ['ngSanitize'])
+		.controller('WikiController', WikiController)
+		.directive('autofocus', ['$timeout', autofocus]);
 
-    function WikiController($http, $window) {
+	function WikiController($http, $window) {
 
-        var wiki = this;
-        wiki.apiUrl = updateBaseUrl();
-        wiki.lang = 'en';
-        wiki.domain = 'wikipedia';
-        wiki.apiUrl = 'http://' + wiki.lang + '.' + wiki.domain + '.org/w/api.php';
-        wiki.term = 'zen'; // default
-        wiki.searchFilter = "intitle:";
-        wiki.page = null;
-        wiki.results = null;
-        wiki.error = "";
-        wiki.leadLarge = false;
+		var wiki = this;
+		wiki.apiUrl = updateBaseUrl();
+		wiki.lang = 'en';
+		wiki.domain = 'wikipedia';
+		wiki.apiUrl = 'http://' + wiki.lang + '.' + wiki.domain + '.org/w/api.php';
+		wiki.term = 'zen'; // default
+		wiki.searchFilter = "intitle:";
+		wiki.page = null;
+		wiki.results = null;
+		wiki.error = "";
+		wiki.leadLarge = false;
 
-        // TODO: dinamicaly populate list with available languages
-        wiki.languages = [
-        {
-            id: 'en',
-            name: 'English'
+		// TODO: dinamicaly populate list with available languages
+		wiki.languages = [{
+			id: 'en',
+			name: 'English'
         }, {
-            id: 'sr',
-            name: 'Српски'
+			id: 'sr',
+			name: 'Српски'
         }, {
-            id: 'sh',
-            name: 'Srpskohrvatski'
+			id: 'sh',
+			name: 'Srpskohrvatski'
         }]; // languages
 
-        wiki.pageParams = {
-            titles: wiki.term
-        };
+		wiki.pageParams = {
+			titles: wiki.term
+		};
 
-        wiki.searchParams = {
-            generator: 'search',
-            gsrsearch: wiki.term + wiki.searchFilter,
-            gsrlimit: 20, // broj rezultata, max 50
-            pilimit: 'max', // thumb image for all articles
-            exlimit: 'max', // extract for all articles
-            imlimit: 'max', // images in articles
-            exintro: '' // extracts intro
-        };
+		wiki.searchParams = {
+			generator: 'search',
+			gsrsearch: wiki.term + wiki.searchFilter,
+			gsrlimit: 20, // broj rezultata, max 50
+			pilimit: 'max', // thumb image for all articles
+			exlimit: 'max', // extract for all articles
+			imlimit: 'max', // images in articles
+			exintro: '' // extracts intro
+		};
 
-        // defaul params both for open and search
-        var commonParams = {
-            action: 'query',
-            prop: 'extracts|pageimages|images',
-            redirects: '', // automatically resolve redirects
-            format: 'json',
-            formatversion: 2,
-            callback: 'JSON_CALLBACK'
-        };
-
-
-        /*** PUBLIC METHODS ***/
-
-        wiki.searchWikipedia = function() { // mozda ne treba ulazni argument
-            updateBaseUrl();
-            updateSearchTerm();
-            var paramUrl = createParamUrl(wiki.searchParams);
-
-            $http.jsonp(paramUrl)
-                .success(function(data) {
-                    resetError();
-                    if (!data.query) {
-                        wiki.emptyResults();
-                        return false;
-                    }
-                    wiki.results = data.query.pages;
-                    wiki.openArticle(wiki.term);
-                })
-                .error(handleErrors);
-        }; // searchWikipedia
+		// defaul params both for open and search
+		var commonParams = {
+			action: 'query',
+			prop: 'extracts|pageimages|images',
+			redirects: '', // automatically resolve redirects
+			format: 'json',
+			formatversion: 2,
+			callback: 'JSON_CALLBACK'
+		};
 
 
-        wiki.openArticle = function(title) {
-            if (isPageOpen(title)) {
-                return;
-            }
-            wiki.pageParams.titles = title;
-            var paramUrl = createParamUrl(wiki.pageParams);
+		/*** PUBLIC METHODS ***/
 
-            $http.jsonp(paramUrl)
-                .success(function(data) {
-                    if (!data.query) return;
-                    wiki.page = data.query.pages[0];
-                    removeLeadFromList(title, data.query.redirects);
-                })
-                .error(handleErrors);
-        }; // openArticle
+		wiki.searchWikipedia = function () { // mozda ne treba ulazni argument
+			updateBaseUrl();
+			updateSearchTerm();
+			var paramUrl = createParamUrl(wiki.searchParams);
 
-
-        wiki.searchInDomain = function(domainName) {
-            setDomainName(domainName);
-            wiki.searchWikipedia();
-        }; // searchInDomain
+			$http.jsonp(paramUrl)
+				.success(function (data) {
+					resetError();
+					if (!data.query) {
+						wiki.emptyResults();
+						return false;
+					}
+					wiki.results = data.query.pages;
+					wiki.openArticle(wiki.term);
+				})
+				.error(handleErrors);
+		}; // searchWikipedia
 
 
-        wiki.searchForLeadTerm = function(title) {
-            if (wiki.leadLarge) {
-                wiki.term = title;
-                wiki.searchWikipedia(title, wiki.searchParams);
-            }
-            wiki.toggleLeadLarge();
-        }; // searchForLeadTerm
+		wiki.openArticle = function (title) {
+			if (isPageOpen(title)) {
+				return;
+			}
+			wiki.pageParams.titles = title;
+			var paramUrl = createParamUrl(wiki.pageParams);
+
+			$http.jsonp(paramUrl)
+				.success(function (data) {
+					if (!data.query) return;
+					wiki.page = data.query.pages[0];
+					removeLeadFromList(title, data.query.redirects);
+				})
+				.error(handleErrors);
+		}; // openArticle
 
 
-        wiki.openLarge = function(title) {
-            wiki.page = '';
-            wiki.openArticle(title);
-            wiki.leadLarge = true;
-        }; // openLarge
+		wiki.searchInDomain = function (domainName) {
+			setDomainName(domainName);
+			wiki.searchWikipedia();
+		}; // searchInDomain
 
 
-        wiki.toggleLeadLarge = function() {
-            wiki.leadLarge = !wiki.leadLarge;
-        }; // toggleLeadLarge
+		wiki.searchForLeadTerm = function (title) {
+			if (wiki.leadLarge) {
+				wiki.term = title;
+				wiki.searchWikipedia(title, wiki.searchParams);
+			}
+			wiki.toggleLeadLarge();
+		}; // searchForLeadTerm
 
 
-        wiki.selectText = function() {
-            var text = $window.getSelection().toString();
-            wiki.term = text;
-        }; // toggleLeadLarge
+		wiki.openLarge = function (title) {
+			wiki.page = '';
+			wiki.openArticle(title);
+			wiki.leadLarge = true;
+		}; // openLarge
 
 
-        wiki.leadHoverText = function() {
-            return wiki.leadLarge ? "Search for this term" : "Englarge this article";
-        }; // leadHoverText
+		wiki.toggleLeadLarge = function () {
+			wiki.leadLarge = !wiki.leadLarge;
+		}; // toggleLeadLarge
 
 
-        wiki.emptyResults = function() {
-            wiki.results = [];
-            wiki.page = "";
-        }; // emptyResults
+		wiki.selectText = function () {
+			var text = $window.getSelection().toString();
+			wiki.term = text;
+		}; // toggleLeadLarge
 
 
-        wiki.checkMax = function() {
-            if (wiki.searchParams.gsrlimit > 50) wiki.searchParams.gsrlimit = 50;
-        }; // checkMax
+		wiki.leadHoverText = function () {
+			return wiki.leadLarge ? "Search for this term" : "Englarge this article";
+		}; // leadHoverText
 
 
-        wiki.checkLang = function() {
-            console.log("prov");
-            if (wiki.lang.length < 2) {
-                return false;
-            }
-        }; // checkMax
-
-        /*** HELPER FUNCTIONS ***/
-
-        function setDomainName(domainName) {
-            wiki.domain = domainName;
-        } // setDomainName
-
-        function updateBaseUrl() {
-            wiki.apiUrl = 'http://' + wiki.lang + '.' + wiki.domain + '.org/w/api.php';
-            if (wiki.domain == 'commons') wiki.apiUrl = 'http://commons.wikimedia.org/w/api.php';
-        } // updateBaseUrl
-
-        function updateSearchTerm() {
-            wiki.searchParams.gsrsearch = wiki.searchFilter + wiki.term;
-        } // updateSearchTerm
-
-        function isPageOpen(title) {
-            return (wiki.page && (wiki.page.title == title));
-        } // isPageOpen
-
-        function removeLeadFromList(term, redirects) {
-            for (var x in wiki.results) {
-                if (wiki.results[x].title == capitalizeFirst(term)) {
-                    wiki.results.splice(x, 1); // remove it from the list
-                }
-                if (!redirects) return wiki.results;
-                for (var r in redirects) {
-                    if (redirects[r].to == wiki.results[x].title) {
-                        wiki.results.splice(x, 1);
-                    }
-                }
-            } // end for
-            return wiki.results;
-        } // removeLeadFromList
-
-        function handleErrors(data, status, headers, config) {
-            wiki.error = "Oh no, there was some error in geting data: " + status;
-        } // handleErrors
-
-        function resetError() {
-            wiki.error = "";
-        }
-
-        function createParamUrl(params) {
-            angular.extend(params, commonParams);
-            var paramUrl = wiki.apiUrl + '?' + serialize(params);
-            console.log(paramUrl);
-            return paramUrl;
-        } // createParamUrl
-
-        function serialize(params) {
-            var paramString = Object.keys(params).map(function(key) {
-                return key + '=' + encodeURIComponent(params[key]);
-            }).join('&');
-            return (paramString);
-        } // serialize
-
-        function capitalizeFirst(string) {
-            return string.charAt(0).toUpperCase() + string.slice(1);
-        } // capitalizeFirst
-
-    } // WikiController
+		wiki.emptyResults = function () {
+			wiki.results = [];
+			wiki.page = "";
+		}; // emptyResults
 
 
-    function autofocus($timeout) {
-        return {
-            restrict: 'A',
-            link: function($scope, $element) {
-                $timeout(function() {
-                    $element[0].focus();
-                });
-            }
-        };
-    } // autofocus
+		wiki.checkMax = function () {
+			if (wiki.searchParams.gsrlimit > 50) wiki.searchParams.gsrlimit = 50;
+		}; // checkMax
+
+
+		wiki.checkLang = function () {
+			console.log("prov");
+			if (wiki.lang.length < 2) {
+				return false;
+			}
+		}; // checkMax
+
+		/*** HELPER FUNCTIONS ***/
+
+		function setDomainName(domainName) {
+			wiki.domain = domainName;
+		} // setDomainName
+
+		function updateBaseUrl() {
+			wiki.apiUrl = 'http://' + wiki.lang + '.' + wiki.domain + '.org/w/api.php';
+			if (wiki.domain == 'commons') wiki.apiUrl = 'http://commons.wikimedia.org/w/api.php';
+		} // updateBaseUrl
+
+		function updateSearchTerm() {
+			wiki.searchParams.gsrsearch = wiki.searchFilter + wiki.term;
+		} // updateSearchTerm
+
+		function isPageOpen(title) {
+			return (wiki.page && (wiki.page.title == title));
+		} // isPageOpen
+
+		function removeLeadFromList(term, redirects) {
+			for (var x in wiki.results) {
+				if (wiki.results[x].title == capitalizeFirst(term)) {
+					wiki.results.splice(x, 1); // remove it from the list
+				}
+				if (!redirects) return wiki.results;
+				for (var r in redirects) {
+					if (redirects[r].to == wiki.results[x].title) {
+						wiki.results.splice(x, 1);
+					}
+				}
+			} // end for
+			return wiki.results;
+		} // removeLeadFromList
+
+		function handleErrors(data, status, headers, config) {
+			wiki.error = "Oh no, there was some error in geting data: " + status;
+		} // handleErrors
+
+		function resetError() {
+			wiki.error = "";
+		}
+
+		function createParamUrl(params) {
+			angular.extend(params, commonParams);
+			var paramUrl = wiki.apiUrl + '?' + serialize(params);
+			console.log(paramUrl);
+			return paramUrl;
+		} // createParamUrl
+
+		function serialize(params) {
+			var paramString = Object.keys(params).map(function (key) {
+				return key + '=' + encodeURIComponent(params[key]);
+			}).join('&');
+			return (paramString);
+		} // serialize
+
+		function capitalizeFirst(string) {
+			return string.charAt(0).toUpperCase() + string.slice(1);
+		} // capitalizeFirst
+
+		function createImageUrl(name) { // param: filename as a string
+			var filename = name.replace(/ /g, "_");
+			var digest = md5(filename);
+			var folder = digest[0] + '/' + digest[0] + digest[1] + '/' + encodeURIComponent(filename);
+			var url = 'http://upload.wikimedia.org/wikipedia/commons/' + folder;
+			return url;
+		} // createImageUrl
+
+	} // WikiController
+
+
+	function autofocus($timeout) {
+		return {
+			restrict: 'A',
+			link: function ($scope, $element) {
+				$timeout(function () {
+					$element[0].focus();
+				});
+			}
+		};
+	} // autofocus
 
 })();
