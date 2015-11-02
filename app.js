c2a86402f6cbcd4d06a3a7288517fe51a61661c7
mudroljub <mudroljub@gmail.com>
2015-11-02 17:04:06 +0100

    m

--- app.js
+++ app.js
@@ -135,10 +135,10 @@
 
 		function handleArticle(data) {
 			if (!data.query) return;
-			if (data.query.pages[0].missing) tryAgainCapitalized(Params.getArticleParams().titles);
+			if (data.query.pages[0].missing) tryAgainCapitalized(Params.fullArticleParams().titles);
 			if (data.query.pages[0].missing) return;
 			wiki.page = data.query.pages[0];
-			removeArticleFromResults(Params.getArticleParams().titles, wiki.results);
+			removeArticleFromResults(Params.fullArticleParams().titles, wiki.results);
 			removeRedirections(data.query.redirects, wiki.results);
 			if (wiki.page.pageimage) findWikiImage(wiki.page.pageimage);
 		}	// hande
