610d223cef05c72488142671917aa5fe37f287ca
mudroljub <mudroljub@gmail.com>
2015-11-09 10:36:26 +0100

    findImagePage aplied, delete old helper functions

--- services/Api.js
+++ services/Api.js
@@ -27,7 +27,7 @@
 					api.exactMatch = null;
 					if (!data.query) return;
 					api.results = data.query.pages;
-					findImagePages();
+					angular.forEach(api.results, findImagePage);
 					api.exactMatch = findExactTerm();
 					if (!api.exactMatch) return;
 					api.params.setArticleTitle(api.exactMatch);
@@ -39,17 +39,12 @@
 
 		api.open = function() {
 			var paramUrl = createParamUrl(Params.getArticleParams());
-			//console.log(paramUrl);
 			$http.jsonp(paramUrl)
 				.success(function (data) {
 					api.page = null;
 					if (!data.query) return;
 					api.page = data.query.pages[0];
-					if (api.page.pageimage) {
-						var filename = api.page.pageimage;
-						var thumbUrl = api.page.thumbnail.source;
-						api.page.imageUrl = createImageUrl(filename, thumbUrl);
-					}
+					findImagePage(api.page);
 				})
 				.error(handleErrors);
 		}; // open
@@ -57,12 +52,18 @@
 
         /*** HELPERS ***/
 
-		function createImageUrl(filename, thumbUrl) {
-			var escaped = escape(filename);
-			var substrEnd = thumbUrl.indexOf(escaped) + escaped.length;
-			var newUrl = thumbUrl.substring(0, substrEnd).replace("thumb/", "");
-			return newUrl;
-		}	// createImageUrl
+		function findImagePage(thisResult) {
+			if(thisResult.pageimage) {
+				var imgSrc = thisResult.thumbnail.source;
+				var commonsUrl = "https://upload.wikimedia.org/wikipedia/commons/";
+				var imageName = thisResult.pageimage;
+				if(utils.startsWith(imgSrc, commonsUrl)) {
+					thisResult.imagePage = "https://commons.wikimedia.org/wiki/File:" + 	imageName;
+				} else {
+					thisResult.imagePage = "https://" + api.params.settings.lang + "." + api.params.settings.domain + ".org/wiki/File:" + imageName;
+				}
+			}
+		} // findImagePage
 
         function createParamUrl(params) {
 			var paramUrl = Params.getApiUrl() + '?' + utils.serialize(params);
@@ -83,21 +84,6 @@
 			});
 			return found;
 		}	// findExactTerm
-
-		function findImagePages() {
-			for(var r in api.results) {
-				if(api.results[r].pageimage) {
-					var imgSrc = api.results[r].thumbnail.source;
-					var commonsUrl = "https://upload.wikimedia.org/wikipedia/commons/";
-					var imageName = api.results[r].pageimage;
-					if(utils.startsWith(imgSrc, commonsUrl)) {
-						api.results[r].imagePage = "https://commons.wikimedia.org/wiki/File:" + 	imageName;
-					} else {
-						api.results[r].imagePage = "https://" + api.params.settings.lang + "." + api.params.settings.domain + ".org/wiki/File:" + imageName;
-					}
-				}
-			}	// end for
-		} // findImagePages
 
 		function handleErrors(data, status, headers, config) {
 			api.error = "Oh no, there was some error in geting data: " + status;
