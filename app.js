94eb302e0bbd78aa706f0fedd1c3c4849e0c2df6
mudroljub <mudroljub@gmail.com>
2015-11-10 16:28:10 +0100

    fork button merged in style.css

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict';
 // browserify app.js -o bundle.js
 /*
 	TODO:
@@ -10,7 +11,7 @@
 	BAG:
 	dolazi do greske kada pretrazuje projekte na kojima nema jezik
 */
-'use strict';
+
 var angular = require('angular');
 var ngSanitize = require('angular-sanitize');
 var WikiController = require('./controllers/WikiController');
@@ -23,12 +24,13 @@ var Params = require('./services/Params');
 var Api = require('./services/Api');
 
 
-
 angular
 	.module('wikiModul', [ngSanitize])
+
 	.controller('WikiController', WikiController)
 	.controller('LanguageController', LanguageController)
 	.directive('autofocus', ['$timeout', autofocus])
+
     .factory('utils', utils)
     .factory('StaticData', StaticData)
 	.service('LanguageService', LanguageService)
