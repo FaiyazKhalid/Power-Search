fdecee6bbfd52012da36f6ae705132f1c9e69048
mudroljub <mudroljub@gmail.com>
2015-11-18 13:14:42 +0100

    rename image page to main image

---
+++ components/main-image/MainImageController.js
@@ -0,0 +1,11 @@
+function MainImageController(MainImage, Params) {
+'use strict';
+
+	var mainImgControl = this;
+	mainImgControl.mainImage = MainImage;
+	mainImgControl.params = Params;
+
+
+} // MainImageController
+
+module.exports = MainImageController;
---
+++ components/main-image/mainImage.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function mainImage() {
+	return {
+		controller: 'MainImageController',
+		controllerAs: 'mainImgControl',
+        templateUrl: 'components/main-image/main-image.html'
+	};
+} // mainImage
+
+module.exports = mainImage;
