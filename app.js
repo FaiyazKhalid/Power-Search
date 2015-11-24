591e8ab2dfc4b16cd1ad9621feb4eaf184d926b9
mudroljub <mudroljub@gmail.com>
2015-11-24 17:05:47 +0100

    add img desc

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 'use strict';
 /*
 	TODO:
-	// srediti jezike
 	// srediti opis slika: http://stackoverflow.com/questions/26010257/bootstrap-3-css-image-caption-overlay
+	// dinamicki slati zahtev za sirinom thumb slika, zavisno od sirine ekrana
+	// ubaciti loadmore
+	// srediti jezike
 	// primer paramUrl u dokumentaciju
 	// izbaciti mainControl iz page.html i params.html
 */
