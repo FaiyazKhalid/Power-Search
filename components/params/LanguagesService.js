54a594fd35ae09327686745fffbb3d78d3113f44
mudroljub <mudroljub@gmail.com>
2015-12-07 22:46:18 +0100

    otklonjen bug

--- components/params/LanguagesService.js
+++ components/params/LanguagesService.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// samo jednom treba da ucita sve (mozda na init)
-// proveriti na kom se domenu nalazimo
-// prikazati samo jezike za taj domen (site)
-
 function LanguagesService($http, ParamService) {
 
     var self = this;
@@ -14,17 +10,15 @@ function LanguagesService($http, ParamService) {
 
 	self.get = function() {
         self.resetErrors();
+        if(ParamService.getDomain() === "commons") return;
         var paramUrl = "https://en.wikipedia.org/w/api.php?action=sitematrix&smtype=language&format=json&formatversion=2&callback=JSON_CALLBACK";
         console.log(paramUrl);
+
 		$http.jsonp(paramUrl)
 			.success(function (data) {
                 self.all = [];
                 filterResults(data);
-			})
-            .error(function(){
-                self.error = "The language you requesting does not exist for this domain.";
-                ParamService.setLanguage('en');
-            });
+			});
 	}; // search
 
 
@@ -37,7 +31,7 @@ function LanguagesService($http, ParamService) {
 
 	function filterResults(data) {
         var domain = ParamService.getDomain();
-        if(domain === "wikipedia") domain = "wiki";        
+        if(domain === "wikipedia") domain = "wiki";
         angular.forEach(data.sitematrix, function(item) {
             if(item.site) {
                 for(var i = 0; i < item.site.length; i++) {
