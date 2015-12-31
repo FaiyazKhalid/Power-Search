a18814e2ae82e1ee359a5e1c82c67bbe590b4046
mudroljub <mudroljub@gmail.com>
2015-12-31 12:44:51 +0100

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+
+/*** CSS TASKS ***/
+
+gulp.task('minifyCss', function () {
+	return gulp.src('css/dist/style.css')
+		.pipe(minifyCSS())
+		.pipe(rename('minified.css'))
+		.pipe(gulp.dest('css'));
+}); // minifyCss
